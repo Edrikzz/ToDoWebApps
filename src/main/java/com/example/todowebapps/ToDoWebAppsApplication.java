0b19349d786920e7ec4c065d3f40292845636557
Edrik Tjhie <tjhieem@cardiff.ac.uk>
2022-03-15 14:18:29 +0000

    Initial commit

---
+++ src/main/java/com/example/todowebapps/ToDoWebAppsApplication.java
@@ -0,0 +1,13 @@
+package com.example.todowebapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoWebAppsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoWebAppsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todowebapps/ToDoWebAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todowebapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoWebAppsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
